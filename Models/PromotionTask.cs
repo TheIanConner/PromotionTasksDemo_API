d2fd0788425eabe3a712324b8bcdc59ed36fbfed
Ian <ianconner@outlook.com>
2025-05-06 22:24:17 +0100

    Remove empty redundant Release properties from the PromotionTasks

--- Models/PromotionTask.cs
+++ Models/PromotionTask.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace PromotionTasksService.Models;
 
@@ -23,6 +24,7 @@ public class PromotionTask
     /// <summary>
     /// Gets or sets the release this task belongs to.
     /// </summary>
+    [JsonIgnore]
     public Release? Release { get; set; }
     
     /// <summary>
